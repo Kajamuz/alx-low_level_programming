9c5bc45105f90b12ea7845cdf40fb656f53d74ba
Jamleck Muriuki <jamleckmuriuki@gmail.com>
2023-06-15 17:40:14 -0700

    This is a C program that prints exactly "Programming is like building a multilingual puzzle" followed by a new line. Only the function puts will be used and the program should end with the value 0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main: this is the entry point
+*return: return will always be 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
