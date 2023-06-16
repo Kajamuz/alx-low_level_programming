9b0588647c658e8216d36f69dace97d9368cb66c
Jamleck Muriuki <jamleckmuriuki@gmail.com>
2023-06-15 18:18:53 -0700

    This is a C program that prints the size of various types on the computer it is compiled and run on. The program always returns 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+
 /**
-*main - main entry point
-*return: reurn always 0 (success)
+*main - Entry Point
+*
+*Return: Always 0 (Success)
 */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
