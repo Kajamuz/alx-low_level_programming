4b62128e10a5b02591e0db61730ca445b245f80e
Jamleck Muriuki <jamleckmuriuki@gmail.com>
2023-06-19 11:58:48 -0700

    This is a C program program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,17 @@
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
+
 
 /**
  * main - Entry point of the program
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	const char message[] =
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	ssize_t len = sizeof(message) - 1;
 
-	write(2, message, len);
+	write(1, "and that piece of art is useful\"
+			- Dora Korpar, 2015-10-19\n", 59);
 
-	return (0);
+	return (1);
 }
