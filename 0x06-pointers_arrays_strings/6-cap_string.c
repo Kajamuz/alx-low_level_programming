c932d7168207911dbba69729addb801fe425411d
Jamleck Muriuki <jamleckmuriuki@gmail.com>
2023-06-29 08:36:42 -0700

    This is code for a fuction that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,24 +7,32 @@
  * Return: Pointer to the resulting string.
  */
 char *cap_string(char *str)
+
 {
+
 	int i = 0;
 
 	while (str[i])
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-			i++;
+	while (!(str[i] >= 'a' && str[i] <= 'z'))
+	i++;
 
-		if (str[i - 1] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-		    str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-		    str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-		    str[i] == '(' || str[i] == ')' || str[i] == '{' ||
-		    str[i] == '}' || i == 0)
-		{
-			str[i] -= 32;
-		}
-
-		i++;
+	if (str[i - 1] == ' ' ||
+	str[i - 1] == '\t' ||
+	str[i - 1] == '\n' ||
+	str[i - 1] == ',' ||
+	str[i - 1] == ';' ||
+	str[i - 1] == '.' ||
+	str[i - 1] == '!' ||
+	str[i - 1] == '?' ||
+	str[i - 1] == '"' ||
+	str[i - 1] == '(' ||
+	str[i - 1] == ')' ||
+	str[i - 1] == '{' ||
+	str[i - 1] == '}' ||
+	i == 0)
+	str[i] -= 32;
+	i++;
 	}
 
 	return (str);
