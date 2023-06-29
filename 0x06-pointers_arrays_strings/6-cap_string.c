4a88687ec81c29e01d2500494690b906a023ce86
Jamleck Muriuki <jamleckmuriuki@gmail.com>
2023-06-29 08:23:33 -0700

    This is code for a fuction that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,23 +9,19 @@
 char *cap_string(char *str)
 {
 	int i = 0;
-	int capitalize_next = 1;
 
-	while (str[i] != '\0')
+	while (str[i])
 	{
-		if (capitalize_next && (str[i] >= 'a' && str[i] <= 'z'))
-		{
-			str[i] = str[i] - 32;
-			capitalize_next = 0;
-		}
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
 
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
-		    || str[i] == ',' || str[i] == ';' || str[i] == '.'
-		    || str[i] == '!' || str[i] == '?' || str[i] == '"'
-		    || str[i] == '(' || str[i] == ')' || str[i] == '{'
-		    || str[i] == '}')
+		if (str[i - 1] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+		    str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+		    str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+		    str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+		    str[i] == '}' || i == 0)
 		{
-			capitalize_next = 1;
+			str[i] -= 32;
 		}
 
 		i++;
