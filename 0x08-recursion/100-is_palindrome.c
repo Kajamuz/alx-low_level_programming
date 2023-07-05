04b8c15392b416615950b9837e09bcb7106e808f
Jamleck Muriuki <jamleckmuriuki@gmail.com>
2023-07-05 09:45:40 -0700

    This is code for a fuction that returns 1 if a string is a palindrome and 0 if not.

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,59 +1,33 @@
 #include "main.h"
 
 /**
- * _strlen - Returns a string's length
- * @s: The string whose length to find
+ * is_palindrome - Determines if a string is a palindrome.
+ * @s: String to be inspected.
  *
- * Return: The string's length
- */
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-
-	return (len);
-}
-
-/**
- * is_palindrome_recursive - Recursive function to inspect
- * if string is a palindrome
- *
- * @s: String to check
- * @start: The current comparison's starting index
- * @end: The current comparison's ending index
- *
- * Return: 1 if the string is a palindrome,
- * 0 otherwise
- */
-int is_palindrome_recursive(char *s, int start, int end)
-{
-	if (start >= end)
-		return (1);
-
-	if (s[start] != s[end])
-		return (0);
-
-	return (is_palindrome_recursive(s, start + 1, end - 1));
-}
-
-/**
- * is_palindrome - Determines if a string is a palindrome
- * @s: String to check
- *
- * Return: 1 if the string is a palindrome,
- * 0 otherwise
+ * Return: 1 if @s is a palindrome, 0 if not.
  */
 int is_palindrome(char *s)
 {
-	int len = _strlen(s);
+	int len = 0;
+	char *start, *end;
 
-	if (len <= 1)
+	if (*s == '\0')
 		return (1);
 
-	return (is_palindrome_recursive(s, 0, len - 1));
+	while (s[len] != '\0')
+		len++;
+
+	start = s;
+	end = s + len - 1;
+
+	while (end > start)
+	{
+		if (*start != *end)
+			return (0);
+
+		start++;
+		end--;
+	}
+
+	return (1);
 }
